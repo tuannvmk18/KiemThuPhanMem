2ef84a468b5fc4c04705036d92ab80fa8578f015
NguyenVanMinhTuan <tuan01102000@gmail.com>
2020-10-31 18:16:21 +0700

    ahihi 2

--- src/main/java/ProxyAppTest.java
+++ src/main/java/ProxyAppTest.java
@@ -13,13 +13,13 @@ public class ProxyAppTest {
         app = new ProxyApp();
     }
 
-    @DisplayName("The proxy server should be alive")
+    @DisplayName("Should be to connect proxy success")
     @Test()
     void startWithProxySuccess() {
         assertEquals(0, app.startWithProxy("51.75.147.33", "3128"));
     }
 
-    @DisplayName("The proxy shouldn't be alive")
+    @DisplayName("Shouldn't be to connect proxy success")
     @Test()
     void startWithProxyFailed() {
         assertEquals(-1, app.startWithProxy("", ""));
